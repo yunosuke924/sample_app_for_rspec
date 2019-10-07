8e0379ecc9d550900496f200b24a8302c5987661
yuji91 <yuji91.1227@gmail.com>
2019-10-07 18:28:48 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
+  root to: 'tasks#index'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+  get 'sign_up', to: 'users#new'
+  resources :users
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
