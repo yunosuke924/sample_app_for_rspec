b5ebd558d337dbecd7f094a51b742eacc5f0e819
yuji91 <yuji91.1227@gmail.com>
2019-10-07 18:18:29 +0900

    rails g migration AddUserIdToTasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_091610) do
+ActiveRecord::Schema.define(version: 2019_10_07_091750) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_10_07_091610) do
     t.datetime "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
