3c0ccfd6fbe54996fd710ecfa9e5592c683e26a0
yuji91 <yuji91.1227@gmail.com>
2019-10-07 18:14:01 +0900

    Remove comments

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,18 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
-  # GET /tasks
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1
   def show; end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
   def edit; end
 
-  # POST /tasks
   def create
     @task = Task.new(task_params)
 
@@ -28,7 +23,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
   def update
     if @task.update(task_params)
       redirect_to @task, notice: 'Task was successfully updated.'
@@ -37,7 +31,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
@@ -45,12 +38,10 @@ class TasksController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_task
     @task = Task.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def task_params
     params.require(:task).permit(:title, :content, :status, :priority, :deadline)
   end
