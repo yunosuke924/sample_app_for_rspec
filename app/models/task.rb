084492a2cde3a5f756d05db4bbd26e8e6501f062
YUJI ITO <yuji91.1227@gmail.com>
2020-05-25 16:23:39 +0900

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, uniqueness: true
   validates :status, presence: true
   enum status: { todo: 0, doing: 1, done: 2 }
