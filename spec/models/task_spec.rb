ef50e4c94a99abe9a7fce55d1d71f839d7988a43
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-12 17:46:37 +0900

    タスクスペックの作成

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with attributes' do
+      expect(FactoryBot.build(:task)).to be_valid
+    end
+
+    it 'is invalid without title' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it 'is invalid with a duplicate title' do
+      FactoryBot.create(:task, title: 'duplicate title')
+      task = FactoryBot.build(:task, title: 'duplicate title')
+      task.valid?
+      expect(task.errors[:title]).to include("has already been taken")
+    end
+
+    it 'is valid with another title'  do
+      FactoryBot.create(:task, title: 'first title')
+      task = FactoryBot.build(:task, title: 'second title')
+      expect(task).to be_valid
+    end
+  end
 end
