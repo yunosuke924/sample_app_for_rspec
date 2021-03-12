772c617a5457bd8c6ea09724f4ae76cabae23293
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-12 19:04:32 +0900

    Fix: FactoryBot.の記載を省略

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with attributes' do
-      expect(FactoryBot.build(:task)).to be_valid
+      expect(build(:task)).to be_valid
     end
 
     it 'is invalid without title' do
-      task_without_title = FactoryBot.build(:task, title: nil)
+      task_without_title = build(:task, title: nil)
       task_without_title.valid?
       expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without status' do
-      task_without_status = FactoryBot.build(:task, status: nil)
+      task_without_status = build(:task, status: nil)
       task_without_status.valid?
       expect(task_without_status.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
-      FactoryBot.create(:task, title: 'duplicate title')
-      task_with_duplicate_title = FactoryBot.build(:task, title: 'duplicate title')
+      create(:task, title: 'duplicate title')
+      task_with_duplicate_title = build(:task, title: 'duplicate title')
       task_with_duplicate_title.valid?
       expect(task_with_duplicate_title.errors[:title]).to include("has already been taken")
     end
 
     it 'is valid with another title'  do
-      FactoryBot.create(:task, title: 'first title')
-      task_with_another_title = FactoryBot.build(:task, title: 'second title')
+      create(:task, title: 'first title')
+      task_with_another_title = build(:task, title: 'second title')
       expect(task_with_another_title).to be_valid
     end
   end
