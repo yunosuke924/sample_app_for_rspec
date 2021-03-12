7627ae16cc33c720904d65348f766935d893d7ba
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-12 18:38:35 +0900

    Fix: unique制約のあるカラムにsequenceを使用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email {'test@example.com'}
+    sequence(:email) { |n| "user_#{n}@example.com" }
     password {'password'}
     password_confirmation {'password'}
   end
